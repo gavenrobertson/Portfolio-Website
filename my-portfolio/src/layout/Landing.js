1a04c5bdda1d44c8b328676fee3ced1e94819317
Gaven Robertson <94574064+gavenrobertson@users.noreply.github.com>
2023-04-24 14:52:59 -0700

    Update Landing.js

--- my-portfolio/src/layout/Landing.js
+++ my-portfolio/src/layout/Landing.js
@@ -8,7 +8,7 @@ function Landing() {
             <div className='introContainer'>
                 <h1 className='introH1'>Front-End Web Developer</h1>
                 <div>
-                    <img src='https://placekitten.com/420/300'></img>
+                    <img></img>
                 </div>
             </div>
             <p className='introP'>Hello, My name is Gaven Robertson! I am currently based out of</p>
